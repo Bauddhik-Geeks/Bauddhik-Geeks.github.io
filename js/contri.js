d59920e35e8c05dc78fad5a7f457b5554730a1d7
rhvsingh <singhrajaharshvardhan004@gmail.com>
2021-11-12 03:54:19 +0530

    Fixed auto change nav bar

--- js/contri.js
+++ js/contri.js
@@ -49,33 +49,35 @@ var teamMember = [{
     }
 ];
 
-var contriMember = [{
-        name: 'Keval Vavaliya',
-        image: 'https://avatars.githubusercontent.com/u/72963356?v=4',
-        github: 'https://github.com/kevalvavaliya'
-    }, {
-        name: 'Nitesh Thapliyal',
-        image: 'https://avatars.githubusercontent.com/u/53345517?v=4dis',
-        github: 'https://github.com/Nitesh-thapliyal'
-    },
+var contriMember = [
+    /* {
+            name: 'Keval Vavaliya',
+            image: 'https://avatars.githubusercontent.com/u/72963356?v=4',
+            github: 'https://github.com/kevalvavaliya'
+        }, {
+            name: 'Nitesh Thapliyal',
+            image: 'https://avatars.githubusercontent.com/u/53345517?v=4dis',
+            github: 'https://github.com/Nitesh-thapliyal'
+        },
+        {
+            name: 'Aditya Mangal',
+            image: 'https://github.com/adityamangal1.png?size=460',
+            github: 'https://github.com/adityamangal1'
+        }, {
+            name: 'Piyush Chandel',
+            image: 'https://github.com/Piyush-Codes7.png?size=460',
+            github: 'https://github.com/Piyush-Codes7'
+        },
+        {
+            name: 'Harikrushn Kanani',
+            image: 'https://github.com/harikanani.png?size=460',
+            github: 'https://github.com/harikanani'
+        }, {
+            name: 'Kamlesh Joshi',
+            image: 'https://github.com/kamleshjoshi8102.png',
+            github: 'https://github.com/kamleshjoshi8102'
+        }, */
     {
-        name: 'Aditya Mangal',
-        image: 'https://github.com/adityamangal1.png?size=460',
-        github: 'https://github.com/adityamangal1'
-    }, {
-        name: 'Piyush Chandel',
-        image: 'https://github.com/Piyush-Codes7.png?size=460',
-        github: 'https://github.com/Piyush-Codes7'
-    },
-    {
-        name: 'Harikrushn Kanani',
-        image: 'https://github.com/harikanani.png?size=460',
-        github: 'https://github.com/harikanani'
-    }, {
-        name: 'Kamlesh Joshi',
-        image: 'https://github.com/kamleshjoshi8102.png',
-        github: 'https://github.com/kamleshjoshi8102'
-    }, {
         name: 'Rushil Makvana',
         image: 'https://github.com/rushilmakvana.png',
         github: 'https://github.com/rushilmakvana'
@@ -91,19 +93,21 @@ var contriMember = [{
         name: 'Arnav Sharma',
         image: 'https://github.com/arnav1776.png',
         github: 'https://github.com/arnav1776'
-    }, {
-        name: 'Rajkumar Khistariya',
-        image: 'https://github.com/Rajkumar-justcoder.png',
-        github: 'https://github.com/Rajkumar-justcoder'
-    }, {
-        name: 'Abhi Goyani',
-        image: 'https://github.com/abhigoyani.png',
-        github: 'https://github.com/abhigoyani'
-    }, {
-        name: 'Raja Harsh Vardhan Singh',
-        image: 'https://github.com/rhvsingh.png',
-        github: 'https://github.com/rhvsingh'
-    }, {
+    },
+    /*  {
+            name: 'Rajkumar Khistariya',
+            image: 'https://github.com/Rajkumar-justcoder.png',
+            github: 'https://github.com/Rajkumar-justcoder'
+        }, {
+            name: 'Abhi Goyani',
+            image: 'https://github.com/abhigoyani.png',
+            github: 'https://github.com/abhigoyani'
+        }, {
+            name: 'Raja Harsh Vardhan Singh',
+            image: 'https://github.com/rhvsingh.png',
+            github: 'https://github.com/rhvsingh'
+        }, */
+    {
         name: 'Stuti Mongia',
         image: 'https://github.com/stutimongia2024.png',
         github: 'https://github.com/stutimongia2024'
@@ -132,13 +136,13 @@ var contributors2 = document.querySelectorAll('.contributors')[1];
 for (let i = 0; i < teamMember.length; i++) {
     let divCreate = document.createElement('div');
     divCreate.className = 'contri_box';
-    divCreate.innerHTML = '<div class="contri-card"><div class="contri-card-inner"><div class="contri-card-front"><img src="' + teamMember[i].image + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><span>' + teamMember[i].name + '</span><div> <li><a href="' + teamMember[i].github + '" target="_blank"><i class="fab fa-github"></i></a></li><li><a href="' + teamMember[i].twitter + '" target="_blank"><i class="fab fa-twitter"></i></a></li></div></div></div></div>';
+    divCreate.innerHTML = '<div class="contri-card"><div class="contri-card-inner"><div class="contri-card-front"><img src="' + teamMember[i].image + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><div style="position:relative;transform-style:preserve-3d;"><span>' + teamMember[i].name + '</span><div> <li><a href="' + teamMember[i].github + '" target="_blank"><i class="fab fa-github"></i></a></li><li><a href="' + teamMember[i].twitter + '" target="_blank"><i class="fab fa-twitter"></i></a></li></div></div></div></div></div>';
     contributors1.append(divCreate);
 }
 
 for (let i = 0; i < contriMember.length; i++) {
     let divCreate = document.createElement('div');
     divCreate.className = 'contri_box';
-    divCreate.innerHTML = '<div class="contri-card"><div class="contri-card-inner"><div class="contri-card-front"><img src="' + contriMember[i].image + '" alt="' + contriMember[i].name + '" /></div><div class="contri-card-back"><span>' + contriMember[i].name + '</span><div> <li><a href="' + contriMember[i].github + '" target="_blank"><i class="fab fa-github"></i></a></li></div></div></div></div>';
+    divCreate.innerHTML = '<div class="contri-card"><div class="contri-card-inner"><div class="contri-card-front"><img src="' + contriMember[i].image + '" alt="' + contriMember[i].name + '" /></div><div class="contri-card-back"><div><span>' + contriMember[i].name + '</span><div> <li><a href="' + contriMember[i].github + '" target="_blank"><i class="fab fa-github"></i></a></li></div></div></div></div></div>';
     contributors2.append(divCreate);
 }
