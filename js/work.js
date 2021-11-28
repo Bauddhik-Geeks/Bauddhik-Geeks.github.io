00d3f220463eaa51448e95f5da418f8dae0a429d
Rajkumar-stackcoder <meet.r.graphicat10@gmail.com>
2021-11-28 09:24:30 +0530

    File changed and Create saperate files

--- js/work.js
+++ js/work.js
@@ -2,19 +2,19 @@ const workshopcard = [
     {
          workshopname:'Git & Github',
         workshopimg:'github_session.png',
-        workshoplink:''
+        workshoplink:'#' //git and github/git-and-github.html
     },{
         workshopname:'Hacktoberfest',
         workshopimg:'hacktoberfest.png',
-        workshoplink:''
+        workshoplink:'#'
     },{
         workshopname:'Bug Bounty KickStart',
         workshopimg:'web_security_bb.png',
-        workshoplink:''
+        workshoplink:'#'
     },{
          workshopname:'Crypto and Blockchain',
         workshopimg:'Binance_BUIDLer_Program_3.png',
-        workshoplink:''
+        workshoplink:'#'
     },
 ];
 
@@ -23,9 +23,9 @@ for(let i=0;i<workshopcard.length;i++){
     let work=document.createElement('div');
     work.className='workcolums';
     let imgurl;
-   
+  
         imgurl= 'images/' + workshopcard[i].workshopimg;
-        work.innerHTML='<a class="sess_card"><div class="sesscardstext">'+workshopcard[i].workshopname+'</div><div class="sesscardsimg"><img class="sess_img" src="'+imgurl+'" alt="'+workshopcard[i].workshopname+'"></div></a>';
+        work.innerHTML='<a href="'+workshopcard[i].workshoplink + '" class="sess_card"><div class="sesscardstext">'+workshopcard[i].workshopname+'</div><div class="sesscardsimg"><img class="sess_img" src="'+imgurl+'" alt="'+workshopcard[i].workshopname+'"></div></a>';
     
 workRow.append(work);    
 }
